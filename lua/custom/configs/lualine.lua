e7c19de60760ca26b18a8e35a47255238ad3513b
Dylan Pina <77121344+DylanPina@users.noreply.github.com>
2024-03-16 15:12:47 -0400

    Added LSP indicator for Lualine

--- lua/custom/configs/lualine.lua
+++ lua/custom/configs/lualine.lua
@@ -3,6 +3,21 @@ lualine_require.require = require
 
 local icons = require("custom.configs.icons").icons
 
+local clients_lsp = function ()
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  local clients = vim.lsp.buf_get_clients(bufnr)
+  if next(clients) == nil then
+    return ''
+  end
+
+  local c = {}
+  for _, client in pairs(clients) do
+    table.insert(c, client.name)
+  end
+  return '\u{f085} ' .. table.concat(c, '|')
+end
+
 vim.o.laststatus = vim.g.lualine_laststatus
 local opts = {
   options = {
@@ -44,6 +59,7 @@ local opts = {
       { "vim.fn.expand('%:p:~:.')" },
     },
     lualine_x = {
+      clients_lsp,
       {
         "filetype",
         icons_enabled = true,
