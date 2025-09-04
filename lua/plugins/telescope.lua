7e257ad23b700332e5ca9b064f9b3a34b71fa4c4
Dylan Pina <dylansp.dev@gmail.com>
2025-09-03 20:26:45 -0500

    Added lsp reference key bind

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -94,6 +94,7 @@ return {
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<CR>', { desc = 'LSP References' })
 
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to Telescope to change the theme, layout, etc.
